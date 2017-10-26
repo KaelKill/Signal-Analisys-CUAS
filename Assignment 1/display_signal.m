d72de091f0acf61e98939c238ce27a297b2938aa
Kael Kill <kael.r.kill@gmail.com>
2017-10-26 02:53:49 +0200

    Power spectrum

--- Assignment 1/display_signal.m
+++ Assignment 1/display_signal.m
@@ -2,7 +2,10 @@ function display_signal(filename)
     % Display waveform, magnitude, phase and 
     % power spectrum for a input signal
   
-    load(filename,'x','fs');
+    close all, clc
+    
+%     load(filename,'x','fs');
+    [x, fs] = audioread(filename);
     N = length(x); % number of samples
 
     timestep = 1/fs;
@@ -16,25 +19,34 @@ function display_signal(filename)
     Xamp = 2*Xamp;
     Xamp(1) = Xamp(1)/2;
 
-    Xphase = angle(round(X(1:N/2),6));
+    Xphase = angle(round(X(1:N/2),6))*180/pi;
     k = 0:N/2-1;
     f = k*fs/N;
     subplot(2,1,1)
-    plot(f,Xamp),title('Amplitude Spectrum')
+    stem(f,Xamp),title('Amplitude Spectrum')
     xlabel('f (Hz)'),ylabel('|X(f)|'),
     subplot(2,1,2)
     stem(f,Xphase),title('Phase Spectrum')
     xlabel('f (Hz)'),ylabel('Phase(f)')
 
     figure
-    Xpower = Xamp.^2;
-    Xpowerdb = 10*log10(Xamp);
+    Xpower = (Xamp.^2);
+    Xpowerdb = mag2db(Xpower);
+    pmin = min(Xpowerdb)
+    pmax = max(Xpowerdb)
+    for i = 1:length(Xpowerdb)
+        if(Xpowerdb(i) < 0)
+            Xpowerdb(i) = 0;
+        end
+    end
+    
     subplot(2,1,1)
-    plot(f,Xpower),title('Power Spectrum')
+    stem(f,Xpower),title('Power Spectrum')
     xlabel('f (Hz)'),ylabel('Power')
     subplot(2,1,2)
-    plot(f,Xpowerdb),title('Power Spetrum - dB')
-    xlabel('f (Hz)'),ylabel('Power/db')
+    semilogy(f,Xpowerdb,'o'),title('Power Spetrum - dB')
+    axis([0 N round(pmax/10, 3) 2*ceil(pmax)])
+    xlabel('f (Hz)'),ylabel('Power/db'),grid on
     ax = gca;
     ax.YAxis.TickLabelFormat = '%g/dB'; 
-end
+  end
--- Assignment 1/lab2_1.m
+++ Assignment 1/lab2_1.m
@@ -7,8 +7,7 @@ fs = 400;
 dt = 1/fs;
 n = 0:dt:199*dt;
 
-x = 3*sin(2*pi*50*n);
-x = 3*sin(2*pi*50*n);
+x = 330*sin(2*pi*50*n) + 22*sin(2*pi*70*n) + 154*sin(2*pi*20*n);
 
 plot(n, x)
 
